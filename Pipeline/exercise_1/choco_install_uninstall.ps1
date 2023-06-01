1655f1c6149edacc5ffd00136b213005a8a89812
Luis <61985117+luisvelamorilla@users.noreply.github.com>
2023-06-01 12:30:13 +0200

    dia4

--- Pipeline/exercise_1/choco_install_uninstall.ps1
+++
@@ -1,6 +0,0 @@
-$ACTION_PARAM=$args[0]
-$PROGRAMS_PARAM=$args[1]
-$OPTIONS_PARAM=$args[2]
-
-$PROGRAMS = $PROGRAMS_PARAM -Split ' '
-choco $ACTION_PARAM $PROGRAMS $OPTIONS_PARAM -y --force                      
